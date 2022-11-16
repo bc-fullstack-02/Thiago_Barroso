6fe301dc503e252f0f22f5afcee64bd6839f6b28
thiagobarroso <thiagoo.silva@live.com>
2022-11-16 02:41:16 -0300

    FileSystem Express

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
+
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-
+app.use ('/', express.static(__dirname + "/static"))
 app.listen(3000)
+
